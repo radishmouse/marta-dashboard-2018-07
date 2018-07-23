1075f359b0a19d7c76bb998024ca80e1f4a2afcd
Chris Aquino <chris.aquino@gmail.com>
2018-07-23 11:30:48 -0400

    adds basic marta data fetching in custom component

---
+++ src/MartaDashboard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const MARTA_URL = 'https://my-little-cors-proxy.herokuapp.com/http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1';
+
+class MartaDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      blah: 'hey'
+    };
+  }
+
+  componentDidMount() {
+    console.log('about to fetch!');
+    fetch(MARTA_URL, {
+      method: 'get'
+    }).then((response) => {
+        console.log('got the response');
+        return response.json()
+    }).then((jsonData) => {
+        console.log(jsonData);
+        console.log('got the data');
+        this.setState({
+          data: jsonData
+        }, () => {
+          console.log('yeah. you should see data now');
+        });
+    }).catch((err) => {
+        // Error :(
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Marta! Why you late all the time???
+      </div>
+    );
+  }
+}
+
+export default MartaDashboard;
